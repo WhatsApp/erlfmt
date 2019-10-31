e7e06f0c77513f72e1d6a45568cb95cc39b93df4
Michał Muskała <micmus@whatsapp.com>
2019-10-31 09:11:46 -0700

    Introduce metric API to erlfmt_algebra
    Summary:
    This introduces another API in erlfmt_algebra, documented below in code.
    
    The properties were refactored to support an abstract "layout" type,
    since we'll have multiple types wth the same API that should have the
    same properties.
    
    Test Plan: `rebar3 ct erlfmt`
    
    Reviewers: ilyaklyuchnikov, #erl2
    
    Reviewed By: ilyaklyuchnikov
    
    Differential Revision: https://phabricator.intern.facebook.com/D18118000
    
    Tasks: T53582081
    
    Tags: erl2
    
    Signature: 18118000:1572277700:147b6e1cee058bc56608dda80639c99c6f28aa37

--- doc/erlfmt_algebra.md
+++ doc/erlfmt_algebra.md
@@ -15,9 +15,25 @@ through the `lines_combine` function. Vertical composition can be achieved by co
 The `combine` operator works by positionling the right operand to the bottom and right of the
 left operand, for example:
 
-    xxxxxxxx      yyyy         xxxxxxxx
-    xxx       <>  yyyyyyyy  =  xxx
+    xxxxxxxx      yyyyyyyy     xxxxxxxx
+    xxx       <>  yyyy      =  xxx
     xxxxxxx                    xxxxxxx
-    xxxxx                      xxxxxyyyy
-                                    yyyyyyyy
+    xxxxx                      xxxxxyyyyyyyy
+                                    yyyy
 
+## `metric()` type
+
+Metric abstracts over concrete layout shapes to keep track of only 3 measures - the height,
+the maximum width and the width of the last line of the layout. With those 3 measures we can
+order layouts as to how desirable they are - out goal is to produce the smallest layout possible.
+The `metric_dominates/2` function defines a partial order over layouts that will later allow us to
+eliminate some layouts from computation to limit the complexity of the algorithm.
+
+The metric type defines the same operators as the lines type. Building on the previous example,
+doing the same composition with metrics would yield:
+
+    xxxxxxxx      xxxxxxxx     xxxxxxxxxxxxx
+    xxxxxxxx  <>  xxxx      =  xxxxxxxxxxxxx
+    xxxxxxxx                   xxxxxxxxxxxxx
+    xxxxx                      xxxxxxxxxxxxx
+                               xxxxxxxxx
--- src/erlfmt_algebra.erl
+++ src/erlfmt_algebra.erl
@@ -2,8 +2,9 @@
 
 -export([string_new/1, string_append/2, string_spaces/1, string_text/1, string_length/1]).
 -export([lines_new/1, lines_combine/2, lines_flush/1, lines_render/1]).
+-export([metric_new/1, metric_combine/2, metric_flush/1, metric_render/1, metric_dominates/2]).
 
--export_type([text/0, str/0, lines/0]).
+-export_type([text/0, str/0, lines/0, metric/0]).
 
 -type text() :: unicode:chardata().
 
@@ -16,6 +17,15 @@
 -record(lines_flush, {lines :: lines()}).
 
 -opaque lines() :: #string{} | #lines_combine{} | #lines_flush{}.
+
+%% Order of fields is important for comparisons!
+-record(metric, {
+    height :: non_neg_integer(),
+    last_width :: non_neg_integer(),
+    max_width :: non_neg_integer()
+}).
+
+-opaque metric() :: #metric{}.
 
 -spec string_new(text()) -> str().
 string_new(Text) -> #string{length = string:length(Text), text = Text}.
@@ -83,3 +93,31 @@ indent_lines(0, Lines) -> Lines;
 indent_lines(N, Lines) ->
     Offset = string_spaces(N),
     [string_append(Offset, Line) || Line <- Lines].
+
+-spec metric_new(str()) -> metric().
+metric_new(#string{length = Length}) ->
+    #metric{height = 0, max_width = Length, last_width = Length}.
+
+-spec metric_combine(metric(), metric()) -> metric().
+metric_combine(Left, Right) ->
+    #metric{
+        height = Left#metric.height + Right#metric.height,
+        max_width = max(Left#metric.max_width, Right#metric.max_width + Left#metric.last_width),
+        last_width = Left#metric.last_width + Right#metric.last_width
+    }.
+
+-spec metric_flush(metric()) -> metric().
+metric_flush(Metric) ->
+    Metric#metric{height = Metric#metric.height + 1, last_width = 0}.
+
+-spec metric_render(metric()) -> text().
+metric_render(Metric) ->
+    Line = lists:duplicate(Metric#metric.max_width, $x),
+    LastLine = lists:duplicate(Metric#metric.last_width, $x),
+    [lists:duplicate(Metric#metric.height, [Line, $\n]) | LastLine].
+
+-spec metric_dominates(metric(), metric()) -> boolean().
+metric_dominates(Metric0, Metric1) ->
+    Metric0#metric.height =< Metric1#metric.height andalso
+        Metric0#metric.last_width =< Metric1#metric.last_width andalso
+        Metric0#metric.max_width =< Metric1#metric.max_width.
