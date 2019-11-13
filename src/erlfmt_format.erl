937a82a012237a8af8dd0eb6adbdfc1842689c71
Michał Muskała <micmus@fb.com>
2019-11-13 06:41:14 -0800

    Handle floats & chars in erlfmt_format
    Summary:
    This adds support for floats & chars to `erlfmt_format`.
    
    Floats are normalised to downcase the exponent marker.
    
    Chars are normalised for the escapes - explcit escapes in source are kept
    as they were, except upcased - `$\xAA` is more readable than `$\xaa`.
    Other escapes are canonicalised to either the short-form (e.g. $\n)
    where available, or to $\xXX, where XX are hex digits.

--- src/erlfmt_format.erl
+++ src/erlfmt_format.erl
@@ -25,8 +25,37 @@
 -spec expr_to_algebra(erlfmt_parse:abstract_form()) -> erlfmt_algebra:document().
 expr_to_algebra({integer, Meta, _Value}) ->
     {text, Text} = proplists:lookup(text, Meta),
-    document_text(format_integer(Text)).
+    document_text(format_integer(Text));
+expr_to_algebra({float, Meta, _Value}) ->
+    {text, Text} = proplists:lookup(text, Meta),
+    document_text(format_float(Text));
+expr_to_algebra({char, Meta, Value}) ->
+    {text, Text} = proplists:lookup(text, Meta),
+    document_text(format_char(Text, Value)).
 
 %% TODO: handle underscores once on OTP 23
 format_integer([B1, B2, $# | Digits]) -> [B1, B2, $# | string:uppercase(Digits)];
 format_integer(Other) -> Other.
+
+%% TODO: handle underscores in int part on OTP 23
+format_float(FloatText) ->
+    [IntPart, DecimalPart] = string:split(FloatText, "."),
+    [IntPart, "." | string:lowercase(DecimalPart)].
+
+%% Keep \\x escapes as they were, but upcased, special-case space in chars vs strings
+format_char("$\\x" ++ Escape, _)  -> ["$\\x" | string:uppercase(Escape)];
+format_char(_, $\s) -> "$\\s";
+format_char(_, Value) -> [$$ | escape_char(Value)].
+
+escape_char(Char) when Char > $\s, Char =< $~; Char > 16#9F -> [Char];
+escape_char($\0) -> "\\0";
+escape_char($\n) -> "\\n";
+escape_char($\r) -> "\\r";
+escape_char($\t) -> "\\t";
+escape_char($\v) -> "\\v";
+escape_char($\b) -> "\\b";
+escape_char($\f) -> "\\f";
+escape_char($\e) -> "\\e";
+escape_char($\d) -> "\\d";
+escape_char(Char) when Char =< 16#9F ->
+    ["\\x" | string:pad(integer_to_binary(Char, 16), 2, leading, $0)].
