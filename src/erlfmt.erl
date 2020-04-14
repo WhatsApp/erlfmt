014a1892205a6162fc767191c2a3b4b4faf3ef9a
Michał Muskała <micmus@whatsapp.com>
2020-04-14 07:04:55 -0700

    Add new CLI, provide rebar3 plugin
    Summary:
    This overhauls the CLI to be more intuitive. Additionaly we also provide
    a rebar3 plugin for easy use in most projects.

--- src/erlfmt.erl
+++ src/erlfmt.erl
@@ -19,21 +19,13 @@
 -typing([dialyzer]).
 
 %% API exports
--export([main/1, format_file/2, read_forms/1, read_forms_string/2, format_error/1, format_error_info/1]).
+-export([main/1, init/1, format_file/2, read_forms/1, read_forms_string/2, format_error/1, format_error_info/1]).
 
--export_type([error_info/0]).
+-export_type([error_info/0, out/0]).
 
--type error_info() :: {file:filename(), erl_anno:location(), module(), Reason :: any()}.
+-type error_info() :: {file:name_all(), erl_anno:location(), module(), Reason :: any()}.
 
--type option() :: verbose | {out, file:name_all()}.
-
--record(state, {
-    verbose = false :: boolean(),
-    out = replace :: file:name_all() | replace,
-    files = [] :: [file:name_all()],
-    warnings = [] :: [error_info()],
-    errors = [] :: [error_info()]
-}).
+-type out() :: standard_out | {path, file:name_all()} | replace.
 
 -define(PAGE_WIDTH, 92).
 
@@ -46,115 +38,47 @@
 %% escript entry point
 -spec main([string()]) -> no_return().
 main(Argv) ->
-    State = parse_args(Argv, #state{}),
-    try run_format(State) of
-        #state{errors = [], warnings = []} ->
-            erlang:halt(0);
-        #state{errors = [], warnings = Warnings} ->
-            [print_warning(Warning) || Warning <- Warnings],
-            erlang:halt(0);
-        #state{errors = Errors, warnings = Warnings} ->
-            [print_warning(Warning) || Warning <- Warnings],
-            [print_error(Error) || Error <- Errors],
-            erlang:halt(1)
-    catch
-        Class:Error:Stack ->
-            io:format(standard_error, "internal error while running ~s:\n", [?COMMAND_NAME]),
-            erlang:raise(Class, Error, Stack)
+    application:ensure_all_started(erlfmt),
+    Opts = erlfmt_cli:opts(),
+    case getopt:parse(Opts, Argv) of
+        {ok, {ArgOpts, _Extra}} ->
+            erlfmt_cli:do(ArgOpts, "erlfmt");
+        {error, Error} ->
+            io:put_chars(standard_error, [getopt:format_error(Opts, Error), "\n\n"]),
+            getopt:usage(Opts, "erlfmt")
     end.
+
+%% rebar3 plugin entry point
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    rebar3_fmt_prv:init(State).
 
 %% API entry point
--spec format_file(file:name_all(), [option()]) -> {ok, [error_info()]} | {error, [error_info(), ...], [error_info()]}.
-format_file(Path, Opts) ->
-    State = parse_opts(Opts, #state{files = [Path]}),
-    case run_format(State) of
-        #state{errors = [], warnings = Warnings} ->
-            {ok, Warnings};
-        #state{errors = Errors, warnings = Warnings} ->
-            {error, Errors, Warnings}
-    end.
-
-print_usage() ->
-    io:format("~s", [[
-"Usage: ", ?COMMAND_NAME, " [-vh] [-o Path] [files...]
-
-", ?COMMAND_NAME, " is a code formatter for Erlang.
-
-Arguments:
-
-  files -- files to format
-
-Options:
-
-  -v -- verbose mode
-
-  -o Path -- output path (default \".\" replacing original files)
-
-  --help (-h) -- print this help message
-
-"
-    ]]).
-
-parse_args(["-v" | Rest], State) ->
-    parse_args(Rest, State#state{verbose = true});
-parse_args(["-o", Path | Rest], State) ->
-    parse_args(Rest, State#state{out = Path});
-parse_args([Help | _Rest], _State) when Help =:= "-h"; Help =:= "--help" ->
-    print_usage(),
-    erlang:halt(0);
-parse_args([Name | Rest], State0) ->
-    State = State0#state{files = [Name | State0#state.files]},
-    parse_args(Rest, State);
-parse_args([], #state{files = []}) ->
-    print_usage(),
-    erlang:halt(0);
-parse_args([], State) ->
-    State#state{files = lists:reverse(State#state.files)}.
-
-parse_opts([verbose | Rest], State) ->
-    parse_opts(Rest, State#state{verbose = true});
-parse_opts([{out, Path} | Rest], State) ->
-    parse_opts(Rest, State#state{out = Path});
-parse_opts([], State) ->
-    State.
-
-run_format(State) ->
-    #state{errors = Errors, warnings = Warnings} =
-        lists:foldl(fun run_format_file/2, State, State#state.files),
-    #state{errors = lists:reverse(Errors), warnings = lists:reverse(Warnings)}.
-
-run_format_file(FileName, State0) ->
-    case State0#state.verbose of
-        true -> io:format(standard_error, "Formatting ~s\n", [FileName]);
-        false -> ok
-    end,
+-spec format_file(file:name_all(), out()) -> {ok, [error_info()]} | {error, error_info()}.
+format_file(FileName, Out) ->
     try
-        {Forms, State1} = read_forms(FileName, State0),
+        {ok, Forms, Warnings} = file_read_forms(FileName),
         [$\n | Formatted] = format_forms(Forms),
         verify_forms(FileName, Forms, Formatted),
-        State2 = write_forms(FileName, Formatted, State1),
-        State2
+        write_forms(FileName, Formatted, Out),
+        {ok, Warnings}
     catch
-        {error, Error} ->
-            #state{errors = [Error | State0#state.errors]}
+        {error, Error} -> {error, Error}
     end.
 
 %% API entry point
 -spec read_forms(file:name_all()) ->
     {ok, [erlfmt_parse:abstract_form()], [error_info()]} | {error, error_info()}.
 read_forms(FileName) ->
-    try read_forms(FileName, #state{}) of
-        {Forms, #state{errors = [], warnings = Warnings}} ->
-            {ok, Forms, Warnings}
+    try file_read_forms(FileName)
     catch
-        {error, Error} ->
-            {error, Error}
+        {error, Error} -> {error, Error}
     end.
 
-read_forms(FileName, State) ->
+file_read_forms(FileName) ->
     case file:open(FileName, [read, {encoding, utf8}]) of
         {ok, File} ->
-            try read_forms(erlfmt_scan:io_form(File), FileName, [], State)
+            try read_forms(erlfmt_scan:io_form(File), FileName, [], [])
             after file:close(File)
             end;
         {error, Reason} ->
@@ -165,32 +89,28 @@ read_forms(FileName, State) ->
 -spec read_forms_string(file:name_all(), string()) ->
     {ok, [erlfmt_parse:abstract_form()], [error_info()]} | {error, error_info()}.
 read_forms_string(FileName, String) ->
-    try read_forms(erlfmt_scan:string_form(String), FileName, [], #state{}) of
-        {Forms, #state{warnings = Warnings}} ->
-            {ok, Forms, Warnings}
+    try read_forms(erlfmt_scan:string_form(String), FileName, [], [])
     catch
-        {error, Error} ->
-            {error, Error}
+        {error, Error} -> {error, Error}
     end.
 
-read_forms({ok, Tokens, Comments, Cont}, FileName, Acc, State0) ->
-    {Form, State} = parse_form(Tokens, Comments, FileName, Cont, State0),
-    read_forms(erlfmt_scan:continue(Cont), FileName, [Form | Acc], State);
-read_forms({eof, _Loc}, _FileName, Acc, State) ->
-    {lists:reverse(Acc), State};
-read_forms({error, {ErrLoc, Mod, Reason}, _Loc}, FileName, _Acc, _State) ->
+read_forms({ok, Tokens, Comments, Cont}, FileName, Acc, Warnings0) ->
+    {Form, Warnings} = parse_form(Tokens, Comments, FileName, Cont, Warnings0),
+    read_forms(erlfmt_scan:continue(Cont), FileName, [Form | Acc], Warnings);
+read_forms({eof, _Loc}, _FileName, Acc, Warnings) ->
+    {ok, lists:reverse(Acc), lists:reverse(Warnings)};
+read_forms({error, {ErrLoc, Mod, Reason}, _Loc}, FileName, _Acc, _Warnings) ->
     throw({error, {FileName, ErrLoc, Mod, Reason}}).
 
-parse_form([], _Comments, _FileName, Cont, State) ->
-    {form_string(Cont), State};
-parse_form(Tokens, Comments, FileName, Cont, State0) ->
+parse_form([], _Comments, _FileName, Cont, Warnings) ->
+    {form_string(Cont), Warnings};
+parse_form(Tokens, Comments, FileName, Cont, Warnings) ->
     case erlfmt_parse:parse_form(Tokens) of
         {ok, Form0} ->
-            {erlfmt_recomment:recomment(Form0, Comments), State0};
+            {erlfmt_recomment:recomment(Form0, Comments), Warnings};
         {error, {ErrLoc, Mod, Reason}} ->
             Warning = {FileName, ErrLoc, Mod, Reason},
-            State = State0#state{warnings = [Warning | State0#state.warnings]},
-            {form_string(Cont), State}
+            {form_string(Cont), [Warning | Warnings]}
     end.
 
 form_string(Cont) ->
@@ -259,28 +179,23 @@ try_location(_, Node) when is_tuple(Node) -> erlfmt_scan:get_anno(location, Node
 try_location(_, [Node | _]) when is_tuple(Node) -> erlfmt_scan:get_anno(location, Node);
 try_location(_, _) -> 0.
 
-write_forms(FileName, Formatted, State) ->
-    OutFileName = out_file(FileName, State),
+write_forms(_FileName, Formatted, standard_out) ->
+    io:write(Formatted);
+write_forms(FileName, Formatted, Out) ->
+    OutFileName = out_file(FileName, Out),
     case filelib:ensure_dir(OutFileName) of
         ok -> ok;
         {error, Reason1} -> throw({error, {OutFileName, 0, file, Reason1}})
     end,
     case file:write_file(OutFileName, unicode:characters_to_binary(Formatted)) of
-        ok -> State;
+        ok -> ok;
         {error, Reason2} -> throw({error, {OutFileName, 0, file, Reason2}})
     end.
 
-out_file(FileName, #state{out = replace}) ->
+out_file(FileName, replace) ->
     FileName;
-out_file(FileName, #state{out = Path}) ->
+out_file(FileName, {path, Path}) ->
     filename:join(Path, filename:basename(FileName)).
-
-%% Keep warnings and errors separate in case we want to colour them
-print_warning(Warning) ->
-    io:put_chars(standard_error, [format_error_info(Warning), $\n]).
-
-print_error(Error) ->
-    io:put_chars(standard_error, [format_error_info(Error), $\n]).
 
 -spec format_error_info(error_info()) -> unicode:chardata().
 format_error_info({FileName, Anno, Mod, Reason}) ->
