fa0901dc5f0c3d98f43f2383d38a120ad63555f3
Michał Muskała <micmus@whatsapp.com>
2020-04-14 07:04:55 -0700

    Add new CLI, provide rebar3 plugin
    Summary:
    This overhauls the CLI to be more intuitive. Additionaly we also provide
    a rebar3 plugin for easy use in most projects.

---
+++ src/rebar3_fmt_prv.erl
@@ -0,0 +1,45 @@
+%% Copyright (c) Facebook, Inc. and its affiliates.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+-module(rebar3_fmt_prv).
+
+-behaviour(provider).
+
+-export([init/1, do/1, format_error/1]).
+
+-define(PROVIDER, fmt).
+-define(DEPS, []).
+
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    Provider = providers:create([
+        {name, ?PROVIDER},
+        {module, ?MODULE},
+        {bare, true},
+        {deps, ?DEPS},
+        {example, "rebar3 fmt"},
+        {opts, erlfmt_cli:opts()},
+        {short_desc, "Erlang code formatter"},
+        {desc, "Erlang code formatter"}
+    ]),
+    {ok, rebar_state:add_provider(State, Provider)}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()}.
+do(State) ->
+    {ArgOpts, _Extra} = rebar_state:command_parsed_args(State),
+    ConfigOpts = rebar_state:get(State, fmt, []),
+    erlfmt_cli:do(ConfigOpts ++ ArgOpts, "rebar3 fmt"),
+    {ok, State}.
+
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
