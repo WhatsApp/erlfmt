0602996bf6e69fc9f1cda4276bb9150e0100f3f5
Walter Schulze <awalterschulze@gmail.com>
2021-03-25 18:05:48 +0000

    fmt

--- src/erlfmt_format.erl
+++ src/erlfmt_format.erl
@@ -315,7 +315,8 @@ binary_op_to_algebra(Op, Meta0, Left0, Right0) ->
 
 rewrite_assoc(Op, MetaA, A, {op, MetaBC, Op, B, C} = BC) ->
     case erlfmt_scan:get_anno(parens, MetaBC, false) of
-        true -> {op, MetaA, Op, A, BC};
+        true ->
+            {op, MetaA, Op, A, BC};
         _ ->
             AB = rewrite_assoc(Op, update_meta_location(MetaBC, A, B), A, B),
             rewrite_assoc(Op, update_meta_location(MetaA, AB, C), AB, C)
