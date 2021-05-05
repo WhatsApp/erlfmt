4cf80bfa85824b2588526efde3024abaf505c131
Walter Schulze <awalterschulze@gmail.com>
2021-05-05 11:18:52 +0100

    update conclusion of analyses for comments with new analyses of comments above module (#284)
    * reproduce analyses on mac
    
    * update conclusions of comments analyses

--- doc/FormattingDecisionComments/module_comments.sh
+++ doc/FormattingDecisionComments/module_comments.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright (c) Facebook, Inc. and its affiliates.
 #
@@ -14,15 +14,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Requires bash version 5
+# $ brew install bash
+
 declare -A repos
 declare -A comments
 
 repos["OTP"]="../../../otp"
 repos["kazoo"]="../../../kazoo"
-repos["Inaka"]="../../../inaka"
+repos["Inaka"]="../../../../inaka"
 repos["MongooseIM"]="../../../MongooseIM"
 repos["ejabberd"]="../../../ejabberd"
-repos["WhatsApp"]="../../../whatsapp/server/erl"
+repos["WhatsApp"]="../../../../../../../local/whatsapp/server/erl"
 
 function sum() {
     local -n array=$1
@@ -36,13 +39,27 @@ function repeat() {
     printf "%$1s" | tr " " $2
 }
 
+function count() {
+    find $1 -name '*.erl' -not -path '**SUITE_data/*' -not -name '*trpc_*' -not -name '*_pb.erl' \
+    | xargs -I {} sh -c 'echo {}; awk "{print} /-module/ {exit}" {}' \
+    | pcre2grep -Mo "^(?:%{$2}(?:\n|[^%\n][^\n]*\n))+" \
+    | pcre2grep -Mv "%% @format\n\n" \
+    | awk 'BEGIN {SUM = 0} !NF {SUM += 1} END { print SUM }'
+}
+
+echo "# Analysis: Number of percentages on module documentation"
+echo "We have conducted a deeper analysis of the number of percentages used for module documentation. This is meant to guide our recommendation of the \`%%% % @format\` module pragma."
+echo "From the empirical results below, it shows that while there is no explicit concensus, most projects prefer using \`%%%\` for module documentation, except for OTP which strongly favors \`%%\`."
+echo "## Methodology"
+echo "We first isolated the comments located above the \`-module\` attribute and counted the number of blocks (as opposed to single lines)"
+echo "for each number of percentage, this avoids skewing the results towards lengthy multi-line comment blocks. We also removed all \`%% @format\` headings to give a better representation of usage."
+echo "We have then outputed as a percentage of total blocks the number of percentages used by each repository."
+echo "## Results"
+
 for repo in ${!repos[*]}; do
     echo $repo
-
     for num_percentage in {1..3}; do
-        comments[$num_percentage]=`eval "find ${repos[$repo]} -name '*.erl' -not -path '**SUITE_data/*' -not -name '*trpc_*' -not -name '*_pb.erl' -print0 \
-        | xargs -0 -I %1 -n1 sh -c 'echo %1; sed -n \"0,/-module/p\" %1 | pcre2grep -Mo \"^(?:%{$num_percentage}(?:\n|[^%\n][^\n]*\n))+\" \
-        | pcre2grep -Mv \"%% @format\n\n\"' | awk 'BEGIN {SUM = 0} !NF {SUM += 1} END { print SUM }'"`
+        comments[$num_percentage]=$(count ${repos[$repo]} $num_percentage)
     done
 
     repo_total=$(sum comments)
@@ -55,3 +72,5 @@ for repo in ${!repos[*]}; do
 
     printf '\n'
 done
+
+echo "Source: [./module_comments.sh](./module_comments.sh)"
