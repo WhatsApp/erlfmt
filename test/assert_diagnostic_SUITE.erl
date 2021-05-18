17f4c1dc7c0d4119071a5be0d8afd218c56b1d54
Yves Gerey <ygy@fb.com>
2021-05-18 11:51:35 +0100

    [Code Cleanup] Turn internal macro helper into function.
    Per code review suggestion, we try to avoid macros as much as possible.

--- test/assert_diagnostic_SUITE.erl
+++ test/assert_diagnostic_SUITE.erl
@@ -20,9 +20,34 @@ groups() ->
 all() ->
     [{group, my_test_group}].
 
+test_equal(_) ->
+    L = [rifi, fifi, loulou],
+    % Must return 'ok' to respect ?assert API.
+    % NB: Must use a temporary result variable, doesn't work inline!
+    Res = ?assertListEqual(L, L),
+    ?assertEqual(ok, Res).
+
+test_expected_is_longer(_) ->
+    L0 = [coffee, brownie],
+    L1 = [coffee],
+    checkListEqualMessage(L0, L1, ["Actual list lacks 1 expected items: [brownie]"]).
+
+test_actual_is_longer(_) ->
+    L0 = [breath],
+    L1 = [breath, explode],
+    checkListEqualMessage(L0, L1, ["Actual list has 1 unexpected items: [explode]"]).
+
+test_one_distinc_item(_) ->
+    L0 = [sea, sax, sun],
+    L1 = [sea, tex, sun],
+    checkListEqualMessage(L0, L1, [
+        "Item 2 differs:\n"
+        "Expected: sax\n"
+        "Value:    tex"
+    ]).
+
 %% Helper ensuring comparison of mismatching lists give the expected message.
-%% Implemented as a macro to have proper line report if the test case fails.
--define(checkListEqualMessage(L0, L1, Msg),
+checkListEqualMessage(L0, L1, Msg) ->
     case (catch ?assertListEqual(L0, L1)) of
         ok ->
             ct:fail("Got 'ok', was expecting exception: ~p", [Msg]);
@@ -35,31 +60,4 @@ all() ->
             ?assertEqual([Msg], Comment);
         X ->
             ct:fail("Expected exception: ~p~nGot: ~p", [Msg, X])
-    end
-).
-
-test_equal(_) ->
-    L = [rifi, fifi, loulou],
-    % Must return 'ok' to respect ?assert API.
-    % NB: Must use a temporary result variable, doesn't work inline!
-    Res = ?assertListEqual(L, L),
-    ?assertEqual(ok, Res).
-
-test_expected_is_longer(_) ->
-    L0 = [coffee, brownie],
-    L1 = [coffee],
-    ?checkListEqualMessage(L0, L1, ["Actual list lacks 1 expected items: [brownie]"]).
-
-test_actual_is_longer(_) ->
-    L0 = [breath],
-    L1 = [breath, explode],
-    ?checkListEqualMessage(L0, L1, ["Actual list has 1 unexpected items: [explode]"]).
-
-test_one_distinc_item(_) ->
-    L0 = [sea, sax, sun],
-    L1 = [sea, tex, sun],
-    ?checkListEqualMessage(L0, L1, [
-        "Item 2 differs:\n"
-        "Expected: sax\n"
-        "Value:    tex"
-    ]).
+    end.
