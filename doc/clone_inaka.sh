7279b4caaf6b52f2e5316185de801052ec88b86f
Tom <TD5@users.noreply.github.com>
2025-11-28 16:26:53 +0000

    Update copyright headers

--- doc/clone_inaka.sh
+++ doc/clone_inaka.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) Meta Platforms, Inc. and its affiliates.
+# Copyright (c) Meta Platforms, Inc. and affiliates.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
